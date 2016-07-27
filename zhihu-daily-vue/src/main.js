9f1057dbcab9714bc2cca2e063f4cac096c2606f
张贯宇 <garychangcn@hotmail.com>
2016-07-28 02:59:41 +0800

    main

---
+++ zhihu-daily-vue/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import vueResource from 'vue-resource';
+import app from './components/app.vue';
+import contents from './components/contents.vue';
+import article from './components/article.vue';
+import index from "./components/index.vue";
+Vue.use(Router);
+Vue.use(vueResource);
+window.Tin="http://zhihu.garychang.cn";
+var router = new Router();
+router.map({
+	'/contents':{
+		component:contents
+	},
+	'/article':{
+		component:article
+	},
+	'/index':{
+		component:index
+	}
+});
+router.redirect({
+	 '*':'/index'
+});
+router.start(app, 'app');
